22ac4d4f6b2805106001de39186e9a087bd1a6f9
Peter Ort <peter@peterort.com>
2020-05-14 18:57:56 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -7,12 +7,10 @@ def actor
 end
 
 def monopoly
-	monopoly = {}
 	monopoly = {railroads: {}}
 end
 
 def monopoly_with_second_tier
-	monopoly = {}
 	monopoly = {railroads:
 								{pieces: 4}
 						 }
