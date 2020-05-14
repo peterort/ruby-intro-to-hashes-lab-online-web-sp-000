0c0fbd83a80cee5794256743874f6657deb6d036
Peter Ort <peter@peterort.com>
2020-05-14 18:56:55 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -13,10 +13,9 @@ end
 
 def monopoly_with_second_tier
 	monopoly = {}
-	monopoly =
-	{railroads:
-		{pieces: 4}
-	}
+	monopoly = {railroads:
+								{pieces: 4}
+						 }
 end
 
 def monopoly_with_third_tier
